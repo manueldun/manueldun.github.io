565ea6353611d8fda66b2fa242fd349e7bb8f8cf
Manuel Dun <manueldun@gmail.com>
2020-02-15 19:46:43 -0400

    does not work from blender export

--- webgl/js/loadJSONmodel.js
+++ webgl/js/loadJSONmodel.js
@@ -1,8 +1,9 @@
-function loadJSONmodel (gl, path) {
+
+function loadJSONmodel(gl, path) {
   var pathToFile = path.match('[A-Za-z0-9]*/[A-Za-z0-9]*/')[0];
 
   var modelJSON = JSON.parse(getString(path));
-  console.log(modelJSON);
+
   var models = [];
 
   for (mesh of modelJSON.meshes) {
@@ -65,7 +66,7 @@ function loadJSONmodel (gl, path) {
   return model;
 }
 
-function loadImage (gl, path) {
+function loadImage(gl, path) {
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
   const level = 0;
@@ -88,4 +89,99 @@ function loadImage (gl, path) {
   };
   image.src = path;
   return texture;
+}
+function loadOBJModel(gl, path, OBJFilename, MTLFilename) {
+  var objStr = getString(path.concat("/").concat(OBJFilename));
+  var importedMeshData = new OBJ.Mesh(objStr);
+  //importedMeshData.calculateTangentsAndBitangents();
+
+  console.log(importedMeshData);
+  
+
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(importedMeshData.vertices), gl.STATIC_DRAW);
+
+  const uvBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(importedMeshData.textures), gl.STATIC_DRAW);
+
+  const normalBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(importedMeshData.vertexNormals), gl.STATIC_DRAW);
+
+  var mtlStr = getString(path.concat("/").concat(MTLFilename));
+  var importedMaterialData = new OBJ.MaterialLibrary(mtlStr);
+
+
+
+  var model = {};
+  model.drawDatas = [];
+
+
+  for (const i in importedMeshData.indicesPerMaterial) {
+    var drawData = {};
+    var materialName = importedMeshData.materialNames[i]
+
+    
+    var difuseFileName = importedMaterialData.materials[materialName].mapDiffuse.filename;
+
+    
+    var difuseMapFullPath = path.concat("/").concat(difuseFileName);
+ 
+    var diffuseTexture = loadImage(gl, difuseMapFullPath);
+    drawData.glDiffuseTexture= diffuseTexture;
+
+    const vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+
+    drawData.vao = vao;
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * Float32Array.BYTES_PER_ELEMENT, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.enableVertexAttribArray(1);
+    gl.vertexAttribPointer(1, 2, gl.FLOAT, gl.FALSE, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.enableVertexAttribArray(2);
+    gl.vertexAttribPointer(2, 3, gl.FLOAT, gl.FALSE, 3 * Float32Array.BYTES_PER_ELEMENT, 0);
+
+    drawData.numberOfIndices = importedMeshData.indicesPerMaterial[i].length;
+
+    const indicesBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(importedMeshData.indicesPerMaterial[i]), gl.STATIC_DRAW);
+
+    
+    model.drawDatas.push(drawData);
+  }
+
+
+  model.draw = function (gl, program, uniforms) {
+    for (uniform of uniforms) {
+      uniform.setUniform();
+    }
+    gl.useProgram(program);
+
+    for (const data of model.drawDatas) {
+
+      gl.activeTexture(gl.TEXTURE0);
+      gl.bindTexture(gl.TEXTURE_2D, data.glDiffuseTexture);
+      
+
+      gl.bindVertexArray(data.vao);
+      gl.drawElements(gl.TRIANGLES, data.numberOfIndices, gl.UNSIGNED_SHORT, 0);
+
+    }
+
+    //gl.activeTexture(gl.TEXTURE0);
+    //gl.bindTexture(gl.TEXTURE_2D, model[i].diffuseTexture);
+
+
+  };
+
+  return model;
 }
